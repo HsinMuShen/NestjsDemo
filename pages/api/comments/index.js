ca1a08231b34d4498c83115439bc12fd0185de50
Michael Shen <electrycity84@gmail.com>
2022-10-20 09:51:18 +0800

    change api file

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,17 +1,21 @@
 import { comments } from "../../../data/comments";
 
 const handler = (req, res) => {
-  if (req.method === "GET") {
-    res.status(200).json(comments);
-  } else if (req.method === "POST") {
-    const comment = req.body.comment;
-    const newComment = {
-      id: Date.now(),
-      text: comment,
-    };
-    comments.push(newComment);
-    res.status(201).json(newComment);
-  }
+  res.status(200).json(comments);
 };
 
 export default handler;
+
+// const handler = (req, res) => {
+//   if (req.method === "GET") {
+//     res.status(200).json(comments);
+//   } else if (req.method === "POST") {
+//     const comment = req.body.comment;
+//     const newComment = {
+//       id: Date.now(),
+//       text: comment,
+//     };
+//     comments.push(newComment);
+//     res.status(201).json(newComment);
+//   }
+// };
